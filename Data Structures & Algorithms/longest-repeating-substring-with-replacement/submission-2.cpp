90b444f155629d34707dfaabed968d0863c4799d
Sujal Pokale <156334934+sujalpokale@users.noreply.github.com>
2026-05-20 14:06:58 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26,0);
+        int l =0;
+        int maxfreq =0;
+        int maxlen =0;
+
+        for(int r =0;r<s.size();r++){
+            freq[s[r] - 'A']++;
+            maxfreq = max(maxfreq,freq[s[r] - 'A']);
+
+            while((r-l+1) - maxfreq > k){
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            maxlen = max(maxlen,r-l+1);
+        }
+        return maxlen;
+    }
+};
