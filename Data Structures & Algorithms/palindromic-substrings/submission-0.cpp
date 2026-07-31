ddbe38279fb2d2082be3927f0fa649bdba38c8fc
Sujal Pokale <156334934+sujalpokale@users.noreply.github.com>
2026-07-31 15:39:45 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int count = 0;
+    void expand(string &s ,int left,int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            count++;
+            left--;
+            right++;
+        }
+    }
+    int countSubstrings(string s) {
+        for(int i =0;i<s.size();i++){
+            expand(s,i,i);
+
+            expand(s,i,i+1);
+        }
+        return count;
+    }
+};
