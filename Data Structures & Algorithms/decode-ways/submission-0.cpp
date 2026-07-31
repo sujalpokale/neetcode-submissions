4af8eb0a157909fda86c8f752c192d424ed6ce5f
Sujal Pokale <156334934+sujalpokale@users.noreply.github.com>
2026-07-31 15:52:26 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+
+        vector<int> dp(n+1,0);
+
+        dp[n] = 1;
+
+        for(int i = n -1;i >= 0;i--){
+            if(s[i] == '0')
+            {
+                dp[i] = 0;
+                continue;
+            }
+
+            dp[i] = dp[ i + 1];
+
+            if(i + 1 < n){
+                int num = (s[i] - '0')*10 + (s[i+1] - '0');
+
+                if(num >= 10 && num <= 26){
+                    dp[i] += dp[i+2];
+                }
+            }
+        }
+
+        return dp[0];
+    }
+};
