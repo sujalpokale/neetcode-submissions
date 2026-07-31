ee8fd34ae614f57e52a44cd3dbb29be647c77201
Sujal Pokale <156334934+sujalpokale@users.noreply.github.com>
2026-07-31 12:19:00 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int robLinear(vector<int>& nums){
+        int n = nums.size();
+
+        if(n == 1)
+            return nums[0];
+        
+        int prev2 = nums[0];
+        int prev1 = max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++){
+            int curr = max(prev1,nums[i] + prev2);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1)
+            return nums[0];
+        
+        vector<int> first(nums.begin(),nums.end() - 1);
+        vector<int> second(nums.begin() + 1,nums.end());
+
+        return max(robLinear(first),robLinear(second));
+    }
+};
